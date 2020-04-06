bf65a0a5941983a80df9682717dcdd83a3fdd83a
yitistica <31217384+yitistica@users.noreply.github.com>
2020-04-06 14:56:26 +0800

    remove 3.5

--- setup.py
+++ setup.py
@@ -21,14 +21,13 @@ if __name__ == '__main__':
     setup(
         author="Yi Q",
         author_email='yitistica@outlook.com',
-        python_requires='>=3.5',
+        python_requires='>=3.6',
         classifiers=[
             'Development Status :: 2 - Pre-Alpha',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Natural Language :: English',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
