9f6c0655974b56911d89f517ffe9613ca1eb8f41
Yi Q <yitistica@outlook.com>
2021-08-25 03:02:51 +0800

    add version

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ if __name__ == '__main__':
         test_suite='tests',
         tests_require=test_requirements,
         url='https://github.com/yitistica/month',
-        version='0.1.0',
+        version='0.1.1',
         zip_safe=False,
     )
