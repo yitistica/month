e7002d9bdc77bc847b14a0c829d32ae0c0f4c386
Yi Q <yitistica@outlook.com>
2021-08-25 03:06:50 +0800

    fix travis error

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ if __name__ == '__main__':
         test_suite='tests',
         tests_require=test_requirements,
         url='https://github.com/yitistica/month',
-        version='0.1.1',
+        version='0.1.2',
         zip_safe=False,
     )
